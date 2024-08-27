f9bdd9a08508d1b21beb54a8a59b457ff4ade645
IgorMHT <igormatheushorn@gmail.com>
2024-08-26 22:28:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
+import random
 app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/testeinicial")
+async def root():
+    return {"It's working!"}
+
+@app.get("/numeroAle")
+async def root():
+    return {"Seu número é:": random.randint(1, 100)}
